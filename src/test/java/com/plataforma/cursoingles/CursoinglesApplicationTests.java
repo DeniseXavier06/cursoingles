66b5b3b1407b2c198454b2c6c11902a102e863fe
DeniseXavier06 <denisexavier062019@gmail.com>
2021-12-30 21:49:48 -0300

    Projeto  criado

---
+++ src/test/java/com/plataforma/cursoingles/CursoinglesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.cursoingles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoinglesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
